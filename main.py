d634faf5e65a8148fc6c60556499fc69049bc5f1
PiotrParyl <piotrekzrydek@gmail.com>
2023-02-22 23:05:58 +0100

    sensor

--- main.py
+++ main.py
@@ -1,18 +1,9 @@
-# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
-# SPDX-License-Identifier: MIT
-
 import time
 import board
 import adafruit_dht
 
-# Initial the dht device, with data pin connected to:
-#dhtDevice = adafruit_dht.DHT22(board.D18)
-
-# you can pass DHT22 use_pulseio=False if you wouldn't like to use pulseio.
-# This may be necessary on a Linux single board computer like the Raspberry Pi,
-# but it will not work in CircuitPython.
+# Set up the DHT22 sensor on GPIO 4
 dhtDevice = adafruit_dht.DHT22(board.D4)
-
 
 while True:
     try:
