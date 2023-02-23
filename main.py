7a62b1d7bbfbf3afe198891d87f82b685eb76bd6
PiotrParyl <piotrekzrydek@gmail.com>
2023-02-23 01:46:28 +0100

    siema

--- main.py
+++ main.py
@@ -10,6 +10,6 @@ while True:
     humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
 
     if humidity is not None and temperature is not None:
-        print("Temp={0:0.1f}*C  Humidity={1:0.1f}%".format(temperature, humidity))
+        print(f"Temperaturka{temperature} Wilgotność{humidity}")
     else:
         print("Failed to retrieve data from humidity sensor")
